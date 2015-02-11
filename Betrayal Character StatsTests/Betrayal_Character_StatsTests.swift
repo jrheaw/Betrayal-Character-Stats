c63ba6d994fc8cd7933f178c68820c2c119cf148
Jacob Wittenauer <jwittenauer@wittenauer-3.local>
2015-02-11 14:16:29 -0600

    Initial Commit

---
+++ Betrayal Character StatsTests/Betrayal_Character_StatsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Betrayal_Character_StatsTests.swift
+//  Betrayal Character StatsTests
+//
+//  Created by Jacob Wittenauer on 2/11/15.
+//  Copyright (c) 2015 Jacob Wittenauer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Betrayal_Character_StatsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
